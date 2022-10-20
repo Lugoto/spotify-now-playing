4d36081169a086c4b5c7d87a3dc15e2935b08d26
bigarmTomi <constipatedfurry@gmail.com>
2022-10-20 17:57:26 +0200

    update example

--- example/index.js
+++ example/index.js
@@ -2,16 +2,27 @@ import { SpotifyService } from 'spotify-now-playing'
 
 const { CLIENT_ID, CLIENT_SECRET, REFRESH_TOKEN } = process.env
 
-const App = async () => {
+const Example = async () => {
     const spotify = new SpotifyService(CLIENT_ID, CLIENT_SECRET, REFRESH_TOKEN)
     const song = await spotify.getCurrentSong()
 
     if(!song.isPlaying) {
-        return console.log('not playing anything')
+        return console.log('not listening to anything')
     }
     
     console.log(`Listening to **${song.title}** by ${song.artist.name}`)
+
+    console.log(`Progress: ${ toMinutes(song.progress )}`)
+    console.log(`Time left: ${ toMinutes(song.trackLength - song.progress) }`)
 }
 
-App()
 
+function toMinutes(ms) {
+    var min = Math.floor((ms / 1000 / 60) << 0)
+    var sec = Math.floor((ms / 1000) % 60)
+
+    return `${min}:${sec}`
+}
+
+
+Example()
